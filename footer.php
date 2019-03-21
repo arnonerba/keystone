f374e0bd66bd57bdf806fb83ab108f7a2ed857b9
Arnon Erba <arnon@arnonerba.com>
2019-03-20 17:59:10 -0700

    Set up basic site layout

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
 <?php ?>
+		</main>
+		<footer id="footer"></footer>
+	</div>
+	<div id="scrim"></div>
 <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -7,3 +7,6 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+	<div id="wrapper">
+		<header id="header"></header>
+		<main id="container">
