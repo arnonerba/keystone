3b9ca90b2b2ee4369c271f427205cbe450ba067d
Arnon Erba <arnon@arnonerba.com>
2019-03-21 22:52:58 -0700

    Create homepage showcase

--- front-page.php
+++ front-page.php
@@ -1,3 +1,18 @@
 <?php get_header(); ?>
 
+	<section id="home-showcase">
+		<div class="flex-columns">
+			<div class="flex-column left">
+				<div class="flex-column-content">
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet tellus id massa molestie rutrum. Cras commodo risus eu accumsan interdum. Sed dapibus tortor quis odio maximus semper. Maecenas rhoncus diam vitae urna tempus, quis vulputate massa fermentum. Proin ultricies sapien nisl, eget pharetra leo bibendum consectetur.
+				</div>
+			</div>
+			<div class="flex-column right">
+				<div class="flex-column-content">
+					test
+				</div>
+			</div>
+		</div>
+	</section>
+
 <?php get_footer(); ?>
