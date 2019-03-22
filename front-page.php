688ddc93494703da675e8e4831442bc8e62d78ed
Arnon Erba <arnon@arnonerba.com>
2019-03-21 22:57:52 -0700

    Add background image to home showcase

--- front-page.php
+++ front-page.php
@@ -7,11 +7,7 @@
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet tellus id massa molestie rutrum. Cras commodo risus eu accumsan interdum. Sed dapibus tortor quis odio maximus semper. Maecenas rhoncus diam vitae urna tempus, quis vulputate massa fermentum. Proin ultricies sapien nisl, eget pharetra leo bibendum consectetur.
 				</div>
 			</div>
-			<div class="flex-column right">
-				<div class="flex-column-content">
-					test
-				</div>
-			</div>
+			<div class="flex-column right" style="background-image:url(<?php header_image(); ?>)"></div>
 		</div>
 	</section>
 
