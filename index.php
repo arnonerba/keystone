0917bf4a5ba221f5a255035652d15f2df8740e23
Arnon Erba <arnon@arnonerba.com>
2019-03-22 21:02:06 -0700

    Add content to single pages

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
 
+	<section id="page-showcase" style="background-image:url(<?php header_image(); ?>)"></section>
+	<div class="container">
+		<section id="page-content">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<h1><?php the_title(); ?></h1>
+				<hr>
+				<?php the_content(); ?>
+			<?php endwhile; endif; ?>
+		</section>
+	</div>
+
 <?php get_footer(); ?>
