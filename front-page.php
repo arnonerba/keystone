eebb9cab5cbb62d743e0bd52a36c306249cf1fb5
Arnon Erba <arnon@arnonerba.com>
2019-03-22 18:28:01 -0700

    Update sample markup

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
 		<div class="flex-columns">
 			<div class="flex-column left">
 				<div class="flex-column-content">
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet tellus id massa molestie rutrum. Cras commodo risus eu accumsan interdum. Sed dapibus tortor quis odio maximus semper. Maecenas rhoncus diam vitae urna tempus, quis vulputate massa fermentum. Proin ultricies sapien nisl, eget pharetra leo bibendum consectetur.
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet tellus id massa molestie rutrum. Cras commodo risus eu accumsan interdum. Sed dapibus tortor quis odio maximus semper. Maecenas rhoncus diam vitae urna tempus, quis vulputate massa fermentum. Proin ultricies sapien nisl, eget pharetra leo bibendum consectetur.</p>
 				</div>
 			</div>
 			<div class="flex-column right" style="background-image:url(<?php header_image(); ?>)"></div>
