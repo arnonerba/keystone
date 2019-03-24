60774e42f88d4ff62d419e0d38fd3f1107221356
Arnon Erba <arnon@arnonerba.com>
2019-03-24 16:13:52 -0700

    Create template for 404 pages

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+	<h1>Uh oh, we can't find that page.</h1>
+	<hr>
+	<p>It looks like that page doesn't exist. Maybe try a search?</p>
+	<?php get_search_form(); ?>
+
+<?php get_footer(); ?>
