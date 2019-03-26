d72f6d3dd01cdd876a43d13cf6bdeb4c62abb32f
Arnon Erba <arnon@arnonerba.com>
2019-03-25 21:10:02 -0700

    Rename template partials directory

--- index.php
+++ index.php
@@ -6,40 +6,40 @@
 	*/
 	if ( is_front_page() && is_home() ) { ?>
 		<h1>Recent Posts</h1>
-		<?php get_template_part( 'templates/blogroll' ); ?>
+		<?php get_template_part( 'partials/blogroll' ); ?>
 	<?php
 	/*
 	Static homepage.
 	*/
 	} elseif ( is_front_page() ) { ?>
-		<?php get_template_part( 'templates/single' ); ?>
+		<?php get_template_part( 'partials/single' ); ?>
 	<?php
 	/*
 	Separate blog page for when homepage is static.
 	*/
 	} elseif ( is_home() ) { ?>
 		<h1><?php single_post_title(); ?></h1>
-		<?php get_template_part( 'templates/blogroll' ); ?>
+		<?php get_template_part( 'partials/blogroll' ); ?>
 	<?php
 	/*
 	Author, Category, Tag, Date, other taxonomy term, or custom post type archive.
 	*/
 	} elseif ( is_archive() ) { ?>
 		<h1><?php the_archive_title(); ?></h1>
-		<?php get_template_part( 'templates/blogroll' ); ?>
+		<?php get_template_part( 'partials/blogroll' ); ?>
 	<?php
 	/*
 	Search results page.
 	*/
 	} elseif ( is_search() ) { ?>
 		<h1>Search Results for "<?php the_search_query(); ?>"</h1>
-		<?php get_template_part( 'templates/blogroll' ); ?>
+		<?php get_template_part( 'partials/blogroll' ); ?>
 	<?php
 	/*
 	Anything else, including single pages and posts.
 	*/
 	} else { ?>
-		<?php get_template_part( 'templates/single' ); ?>
+		<?php get_template_part( 'partials/single' ); ?>
 	<?php } ?>
 
 <?php get_footer(); ?>
