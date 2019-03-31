f6dc902ef38d212ae6752b076b84764f055e5022
Arnon Erba <arnon@arnonerba.com>
2019-03-31 12:12:08 -0700

    Add "featured" text to sticky posts

--- partials/single.php
+++ partials/single.php
@@ -1,7 +1,7 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<?php if ( is_single() ) { ?>
-			<time class="post-date" datetime="<?php echo get_the_date( DATE_W3C ); ?>"><?php echo get_the_date(); ?></time>
+			<p class="post-date"><time datetime="<?php echo get_the_date( DATE_W3C ); ?>"><?php echo get_the_date(); ?></time><?php if ( is_sticky() ) { ?><span class="sticky-post">Featured</span><?php } ?></p>
 		<?php } ?>
 		<h1><?php the_title(); ?></h1>
 		<hr>
