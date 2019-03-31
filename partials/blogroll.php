91cab582afffd251fcecafe90ddcd2e7d5fc7dd4
Arnon Erba <arnon@arnonerba.com>
2019-03-31 14:29:28 -0700

    Use <article> for post/page markup

--- partials/blogroll.php
+++ partials/blogroll.php
@@ -1,13 +1,13 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<hr>
-	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<p class="post-date"><time datetime="<?php echo get_the_date( DATE_W3C ); ?>"><?php echo get_the_date(); ?></time><?php if ( is_sticky() ) { ?><span class="sticky-post">Featured</span><?php } ?></p>
 		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<?php if ( has_post_thumbnail() ) { ?>
 			<figure class="post-thumbnail"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></figure>
 		<?php } ?>
 		<?php the_excerpt(); ?>
-	</div>
+	</article>
 <?php endwhile; else : ?>
 	<hr>
 	<p>No posts found.</p>
--- partials/single.php
+++ partials/single.php
@@ -1,10 +1,10 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<?php if ( is_single() ) { ?>
 			<p class="post-date"><time datetime="<?php echo get_the_date( DATE_W3C ); ?>"><?php echo get_the_date(); ?></time><?php if ( is_sticky() ) { ?><span class="sticky-post">Featured</span><?php } ?></p>
 		<?php } ?>
 		<h1><?php the_title(); ?></h1>
 		<hr>
 		<?php the_content(); ?>
-	</div>
+	</article>
 <?php endwhile; endif; ?>
