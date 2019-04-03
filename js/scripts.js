e63fec84c26a64c5695de9ca4dd78efae5960471
Arnon Erba <arnon@arnonerba.com>
2019-04-02 20:42:49 -0700

    Check tab bar elevation on page load

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
-jQuery(window).scroll(function() {
+function elevateTabBar() {
 	if (jQuery(document).scrollTop() > 0) {
 		jQuery("#tab-bar").addClass("elevated");
 	} else {
 		jQuery("#tab-bar").removeClass("elevated");
 	}
+}
+
+jQuery(document).ready(function() {
+	elevateTabBar();
+});
+
+jQuery(window).scroll(function() {
+	elevateTabBar();
 });
