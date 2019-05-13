6e9f0483085e5bd0cc00f5bfeab2069fb3767531
Arnon Erba <arnon@arnonerba.com>
2019-05-12 22:48:11 -0700

    Enable toggling of navigation drawer

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,27 @@ function elevateTabBar() {
 	}
 }
 
+function showNav() {
+	jQuery("#nav-drawer").toggleClass("toggled");
+	jQuery("#scrim").toggleClass("visible");
+	jQuery(document.body).toggleClass("no-scroll");
+	event.stopPropagation();
+}
+
+function hideNav() {
+	jQuery("#nav-drawer").removeClass("toggled");
+	jQuery("#scrim").removeClass("visible");
+	jQuery(document.body).removeClass("no-scroll");
+}
+
 jQuery(document).ready(function() {
 	elevateTabBar();
+	jQuery(".toggle-nav").click(function(event) {
+		showNav();
+	});
+	jQuery("#scrim").click(function(event) {
+		hideNav()
+	});
 });
 
 jQuery(window).scroll(function() {
