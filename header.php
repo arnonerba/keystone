95a575c799c97f63f61b8f2629949a14249e05bf
Arnon Erba <arnon@arnonerba.com>
2019-10-20 15:04:02 -0700

    Widgetize header

--- header.php
+++ header.php
@@ -32,7 +32,11 @@
 					<div class="flex-columns">
 						<div class="flex-column left">
 							<div class="flex-column-content">
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet tellus id massa molestie rutrum. Cras commodo risus eu accumsan interdum. Sed dapibus tortor quis odio maximus semper. Maecenas rhoncus diam vitae urna tempus, quis vulputate massa fermentum. Proin ultricies sapien nisl, eget pharetra leo bibendum consectetur.</p>
+								<?php if ( is_active_sidebar( 'sidebar-1' ) ) {
+									dynamic_sidebar( 'sidebar-1' );
+								} else {
+									keystone_default_homepage_widget();
+								} ?>
 							</div>
 						</div>
 						<div class="flex-column right" style="background-image:url(<?php header_image(); ?>)"></div>
